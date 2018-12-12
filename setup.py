6dd15b08bf0740ebf99a673c8c5121a7c571a874
tetrau <tetrau01@gmail.com>
2018-12-12 16:49:03 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='munin',
+    version='0.1.0',
+    packages=['munin'],
+    url='https://github.com/tetrau/munin',
+    license='GPLv3',
+    author='tetrau',
+    python_requires='>=3.5.0',
+    install_requires=['requests>=2.0.0'],
+    author_email='tetrau01@gmail.com',
+    description=''
+)
