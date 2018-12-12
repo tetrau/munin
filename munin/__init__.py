dda5932dedbb4fd67d77dadfa81c951449f26c64
tetrau <tetrau01@gmail.com>
2018-12-12 16:29:54 +0800

    first commit

---
+++ munin/__init__.py
@@ -0,0 +1,82 @@
+import requests
+import sqlite3
+import pickle
+import gzip
+import time
+
+
+class Session:
+    def _init_database(self):
+        c = self._connection.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS responses ( \n"
+                  "id INTEGER PRIMARY KEY,\n"
+                  "timestamp REAL,\n"
+                  "url TEXT,\n"
+                  "response BLOB\n"
+                  ")\n")
+        self._connection.commit()
+
+    def __init__(self, database, compress=True):
+        self._compress = compress
+        self._database_path = database
+        self._session = requests.session()
+        self._connection = sqlite3.connect(self._database_path)
+        self._init_database()
+
+    def _insert_response(self, url, response):
+        c = self._connection.cursor()
+        c.execute("INSERT INTO responses (timestamp, url, response) "
+                  "VALUES (?, ?, ?)", (time.time(), url, self._serialize_response(response)))
+        self._connection.commit()
+
+    def _get_response(self, url):
+        c = self._connection.cursor()
+        c.execute("SELECT response FROM responses WHERE url = ? "
+                  "ORDER BY timestamp DESC LIMIT 1", (url,))
+        row = c.fetchone()
+        if not row:
+            return None
+        else:
+            return self._deserialize_response(row[0])
+
+    def __getattr__(self, name):
+        if name == "get":
+            return self._get
+        else:
+            return getattr(self._session, name)
+
+    def _get(self, url, wait=0, use_cache=True, **kwargs):
+        cached_response = self._get_response(url) if use_cache else None
+        if cached_response:
+            cached_response.from_cache = True
+            return cached_response
+        else:
+            response = self._session.get(url, **kwargs)
+            response.from_cache = False
+            self._insert_response(url, response)
+            time.sleep(wait)
+            return response
+
+    def _serialize_response(self, response):
+        pickled_response = pickle.dumps(response)
+        if self._compress:
+            return gzip.compress(pickled_response)
+        else:
+            return pickled_response
+
+    def _deserialize_response(self, s_response):
+        if self._compress:
+            pickled_response = gzip.decompress(s_response)
+        else:
+            pickled_response = s_response
+        return pickle.loads(pickled_response)
+
+    def get_cached_response(self, url):
+        return self._get_response(url)
+
+    def get_all_cached_response(self, url):
+        c = self._connection.cursor()
+        c.execute("SELECT response FROM responses WHERE url = ? "
+                  "ORDER BY timestamp DESC", (url,))
+        rows = c.fetchall()
+        return [self._deserialize_response(r[0]) for r in rows]
---
+++ test/test.py
@@ -0,0 +1,37 @@
+import unittest
+import munin
+import time
+
+
+class TestMunin(unittest.TestCase):
+    def setUp(self):
+        self.session = munin.Session(":memory:")
+
+    def test_simple(self):
+        self.session.headers.update({"User-Agent": "test"})
+        self.assertFalse(self.session.get("https://www.example.com").from_cache)
+        self.assertTrue(self.session.get("https://www.example.com").from_cache)
+
+    def test_cache(self):
+        r = self.session.get("https://www.example.com", wait=1)
+        text = r.text
+        start_time = time.time()
+        for i in range(100):
+            r = self.session.get("https://www.example.com", wait=1)
+            self.assertEqual(r.text, text)
+        self.assertGreater(1, time.time() - start_time)
+
+    def test_get_cache(self):
+        r = self.session.get("https://www.example.com", wait=1)
+        text = r.text
+        cached_r = self.session.get_cached_response("https://www.example.com")
+        self.assertEqual(cached_r.text, text)
+
+    def test_get_multi_cache(self):
+        self.session.get("https://www.example.com", wait=1)
+        self.session.get("https://www.example.com", use_cache=False, wait=1)
+        caches = self.session.get_all_cached_response("https://www.example.com")
+        self.assertEqual(2, len(caches))
+
+    def tearDown(self):
+        self.session.close()
